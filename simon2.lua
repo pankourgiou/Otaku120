cc85dd7fb158f027efc8beda1f4685c1030de14e
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-04-11 23:01:20 +0300

    Update simon2.lua

--- simon2.lua
+++ simon2.lua
@@ -1,3 +1,3 @@
   if (E == mcmod2)  then print("E is true") end --> this will print "true"  
-  if (social)  then print("coffe") end --> this will print "true"
+  if (social)  then print("coffe and croissant") end --> this will print "true"
   if (antisocial)  then print("social") end
